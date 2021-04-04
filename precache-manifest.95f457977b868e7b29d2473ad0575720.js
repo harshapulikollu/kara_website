b724ad0bb69249ec73699e2d1dab54c5b78bba81
harshapulikollu <harshapulikollu@outlook.com>
2021-04-04 13:14:46 +0530

    Updates

---
+++ precache-manifest.95f457977b868e7b29d2473ad0575720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4415985ac5bfc4fcc0e9e1c1afb92389",
+    "url": "/kara_website/index.html"
+  },
+  {
+    "revision": "b86999efc42e3c78e35f",
+    "url": "/kara_website/static/css/main.ba846bd3.chunk.css"
+  },
+  {
+    "revision": "86b5a9dc71e2cabfbd39",
+    "url": "/kara_website/static/js/2.6a2adb3a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kara_website/static/js/2.6a2adb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86999efc42e3c78e35f",
+    "url": "/kara_website/static/js/main.8c18ddbc.chunk.js"
+  },
+  {
+    "revision": "919f73910b74b2cdc9af",
+    "url": "/kara_website/static/js/runtime-main.465bd838.js"
+  }
+]);

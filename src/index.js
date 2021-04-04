06b849fa1c01198815b547082c41fa5be5718e17
harshapulikollu <harshapulikollu@outlook.com>
2021-04-04 14:39:14 +0530

    Modifed baseurl for router

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import {
 
 ReactDOM.render(
   <React.StrictMode>
-     <Router>
+     <Router basename={'/kara_website'}>
       <div>
         {/*
           A <Switch> looks through all its children <Route>

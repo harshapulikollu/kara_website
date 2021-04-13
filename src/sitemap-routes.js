5d1b3e40fe0fcff264883174e18498a07e0f36be
harshapulikollu <harshapulikollu@outlook.com>
2021-04-13 18:24:11 +0530

    Added sitemap for the application

---
+++ src/sitemap-routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+	<Route path='/' />
+	<Route path='/policy/termsofservice' />
+    <Route path='/policy/privacypolicy' />
+    <Route path='/policy/termsofservice' />
+    <Route path='/policy/contentpolicy' />
+    <Route path='/policy/communityguidelines' />
+    <Route path='/faq' />
+    <Route path='/report' />
+    </Route>
+);
+
